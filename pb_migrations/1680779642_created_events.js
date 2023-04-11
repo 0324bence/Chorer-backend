8348b7ef5a63ea2035d41edef3f4ef8c13a61cff
0324bence <bence20050324@gmail.com>
2023-04-11 19:04:46 +0200

    first commit

---
+++ pb_migrations/1680779642_created_events.js
@@ -0,0 +1,93 @@
+migrate((db) => {
+  const collection = new Collection({
+    "id": "xglrodnsed4up60",
+    "created": "2023-04-06 11:14:02.275Z",
+    "updated": "2023-04-06 11:14:02.275Z",
+    "name": "events",
+    "type": "base",
+    "system": false,
+    "schema": [
+      {
+        "system": false,
+        "id": "g4ikdxog",
+        "name": "owner",
+        "type": "relation",
+        "required": false,
+        "unique": false,
+        "options": {
+          "collectionId": "_pb_users_auth_",
+          "cascadeDelete": false,
+          "minSelect": null,
+          "maxSelect": 1,
+          "displayFields": [
+            "username"
+          ]
+        }
+      },
+      {
+        "system": false,
+        "id": "lyvhkxwa",
+        "name": "startTime",
+        "type": "date",
+        "required": true,
+        "unique": false,
+        "options": {
+          "min": "",
+          "max": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "5yxnkszq",
+        "name": "endTime",
+        "type": "date",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": "",
+          "max": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "whqqkxwo",
+        "name": "title",
+        "type": "text",
+        "required": true,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "yshkwi1o",
+        "name": "description",
+        "type": "text",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      }
+    ],
+    "indexes": [],
+    "listRule": null,
+    "viewRule": null,
+    "createRule": null,
+    "updateRule": null,
+    "deleteRule": null,
+    "options": {}
+  });
+
+  return Dao(db).saveCollection(collection);
+}, (db) => {
+  const dao = new Dao(db);
+  const collection = dao.findCollectionByNameOrId("xglrodnsed4up60");
+
+  return dao.deleteCollection(collection);
+})
